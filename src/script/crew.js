ea2e41f2c17932b9cdcdeee0c6215df36435ca4a
marinadiniz-plank <marina.diniz@joinplank.com>
2023-05-10 18:50:01 -0300

    adding cards

---
+++ src/script/crew.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+	var crewList = document.getElementById("crewList");
+	rendercrewList(crewList);
+});
+
+function rendercrewList(crewListDiv) {
+	fetch("http://localhost:80/crew")
+		.then(response => response.json())
+		.then(data => data.forEach(crew => rendercrew(
+			crewListDiv,
+			crew,
+			["list-item", "crew"]
+		))).catch(error => handleRequestError(error, crewListDiv, "crew"));
+}
+
+function rendercrew(parentDiv, crew, crewClasses) {
+	const crewDiv = document.createElement("div");
+	crewClasses.forEach(crewClass => crewDiv.classList.add(crewClass));
+	crewDiv.innerHTML = "" +
+		"<strong>ID:</strong> " + crew.id +
+		"<br><strong>Name:</strong> " + crew.name;
+	parentDiv.appendChild(crewDiv);
+}
+
+function handleRequestError(error, parentDiv, colorClass) {
+	if (parentDiv) {
+		var childDiv = document.createElement("div");
+		childDiv.classList.add("list-item");
+		childDiv.classList.add(colorClass);
+		childDiv.innerHTML = "<strong>Error: </strong> " + error.message;
+		parentDiv.appendChild(childDiv);
+	}
+}
